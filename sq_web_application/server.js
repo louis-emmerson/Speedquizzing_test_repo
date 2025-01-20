8a1c6d51995d3d68ddea8b028982ed6023e8fecf
Patrick Shepherd <patrickshepherd218@gmail.com>
2025-01-20 17:32:03 +0000

    initial start

---
+++ sq_web_application/server.js
@@ -0,0 +1,18 @@
+const WebSocket = require("ws");
+const port = process.env.PORT || 5500;
+const server = new WebSocket.Server({ port: port });
+
+console.log(`WebSocket server started on port ${port}`);
+
+server.on("connection", (socket) => {
+    console.log("New connection");
+    
+    socket.on("message", (msg) => {
+        console.log(msg);
+        server.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(msg);
+        }
+        });
+    });
+});
